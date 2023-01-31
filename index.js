77938014c24a2ccaff1b3eebbaedf9dc68185c6a
Dipro <dipropaul032@gamil.com>
2023-01-31 10:41:38 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const cors = require('cors');
+const app = express()
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+
+app.use(cors())
+app.use(express.json())
+
+const projects = require('./data/projects.json')
+
+
+const uri = `mongodb+srv://ABC123456:ABC123456@cluster0.4k6cglw.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const projectCollection = client.db('PortfolioPorject').collection('projects')
+
+
+        // All Projects
+        app.get('/projects', async (req, res) => {
+            const query = {}
+            const cursor = projectCollection.find(query)
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+        // app.get('/delete', async (req, res)=>{
+        //     const query = {}
+        //     const result = await projectCollection.deleteMany(query)
+        //     res.send(result)
+        // })
+        // Single Projects API
+        app.get('/projects/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await projectCollection.findOne(query)
+            res.send(result)
+        })
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(err => console.log(err))
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello world!');
+})
+
+// app.get('/projects', (req, res) => {
+//     res.send(projects)
+// })
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
